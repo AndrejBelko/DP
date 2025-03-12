559e2972d9f67f5375af27a31ee4c39281f705a4
Andrej <xbelkoa@stuba.sk>
2025-03-12 18:55:05 +0100

    reworked profile

--- show/php/profile.php
+++ show/php/profile.php
@@ -237,10 +237,6 @@ unset($db);
 
     <style>
 
-        .row {
-            padding-top: 20px;
-        }
-
         #map {
             min-height: 400px;
             height: 60%;
@@ -339,65 +335,22 @@ unset($db);
         </div>
     </nav>
 </header>
-<div class="container mt-5">
-    <div class="row text-center justify-content-center">
-        <div class="col-md-6">
-            <button class="mt-4 mb-3 btn btn-primary btn-sm btn-collapse" onclick="showElement('upload-hodinky')">
-                <strong>Upload from smartwatch
-                    (.csv)</strong>
-            </button>
-            <div style="display: none" id="upload-hodinky" class="text-center justify-content-center">
-                <form action="profile.php" method="post" enctype="multipart/form-data">
-                    <input style="width: 250px;" type="file" name="files[]" multiple class="form-control">
-                    <div class="form-group mb-3 d-flex align-items-center">
-                        <label for="trajectoryType" class="form-label me-3">Trajectory type</label>
-                        <div class="form-check form-switch">
-                            <input
-                                    class="form-check-input"
-                                    type="checkbox"
-                                    id="trajectoryType"
-                                    name="trajectoryType"
-                                    value="walk">
-                            <label class="form-check-label" for="trajectoryType">Car Ride</label>
-                        </div>
-                    </div>
-                    <button type="submit" name="submit" class="btn btn-primary">Upload</button>
-                </form>
-            </div>
-        </div>
-        <div class="col-md-6 text-center justify-content-center">
-            <button class="mt-4 mb-3 btn btn-primary btn-sm" onclick="showElement('upload-trex')"><strong>Upload from TRex
-                    (.gpx)</strong>
-            </button>
-            <div style="display: none" id="upload-trex" class="text-center justify-content-center">
-                <form action="profile.php" method="post" enctype="multipart/form-data">
-
-                    <div class="form-group">
-                        <input style="width: 250px;" type="file" name="trexfiles[]" multiple class="form-control">
-                    </div>
-                    <div class="form-group mb-3 d-flex align-items-center">
-                        <label for="trajectoryType" class="form-label me-3">Trajectory type</label>
-                        <div class="form-check form-switch">
-                            <input
-                                    class="form-check-input"
-                                    type="checkbox"
-                                    id="trajectoryType"
-                                    name="trajectoryType"
-                                    value="walk">
-                            <label class="form-check-label" for="trajectoryType">Car Ride</label>
-                        </div>
-                    </div>
-                    <button type="submit" name="trexsubmit" class="btn btn-primary">Upload</button>
-                </form>
-            </div>
-        </div>
+<div class="container">
+    <!-- Buttons for actions aligned on one line with margin between -->
+    <div class="d-flex justify-content-center mt-2">
+        <!-- "Check All" Button -->
+        <button class="mt-4 mb-3 btn btn-primary btn-sm btn-collapse m-3" data-bs-toggle="modal" data-bs-target="#watchUpload">
+            <strong>Upload from smartwatch (.csv)</strong>
+        </button>
+        <button class="mt-4 mb-3 btn btn-primary btn-sm m-3" data-bs-toggle="modal" data-bs-target="#trexUpload">
+            <strong>Upload from TRex (.gpx)</strong>
+        </button>
     </div>
     <form id="actionForm" method="POST" action="profile.php">
-        <div class="container mt-5">
+        <div class="container">
             <div class="row">
                 <div class="col-12 col-md-10 mx-auto">
                     <div class="table-wrapper">
-                        <!-- Buttons for actions aligned on one line with margin between -->
                         <div class="d-flex justify-content-start mt-2">
                             <!-- "Check All" Button -->
                             <button type="button" id="checkAllBtn" class="btn btn-primary btn-sm m-3" onclick="checkAllCheckboxes()">Check All</button>
@@ -519,6 +472,75 @@ unset($db);
     </div>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="watchUpload" tabindex="-1" aria-labelledby="exampleWatchLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleWatchLabel">Upload from smartwatch</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <!-- Modal -->
+            <div class="modal-body">
+                <div id="upload-hodinky" class="text-center justify-content-center mb-2">
+                    <form action="profile.php" method="post" enctype="multipart/form-data">
+                        <input style="width: 250px;" type="file" name="files[]" multiple class="form-control">
+                        <div class="form-group mb-3 d-flex align-items-center">
+                            <label for="trajectoryType" class="form-label me-3">Trajectory type</label>
+                            <div class="form-check form-switch">
+                                <input
+                                        class="form-check-input"
+                                        type="checkbox"
+                                        id="trajectoryType"
+                                        name="trajectoryType"
+                                        value="walk">
+                                <label class="form-check-label" for="trajectoryType">Car Ride</label>
+                            </div>
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-primary">Upload</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="trexUpload" tabindex="-1" aria-labelledby="exampleTrexLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleTrexLabel">Upload from TRex</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <!-- Modal -->
+            <div class="modal-body">
+                <div id="upload-trex" class="text-center justify-content-center mb-2">
+                    <form action="profile.php" method="post" enctype="multipart/form-data">
+
+                        <div class="form-group">
+                            <input style="width: 250px;" type="file" name="trexfiles[]" multiple class="form-control">
+                        </div>
+                        <div class="form-group mb-3 d-flex align-items-center">
+                            <label for="trajectoryType" class="form-label me-3">Trajectory type</label>
+                            <div class="form-check form-switch">
+                                <input
+                                        class="form-check-input"
+                                        type="checkbox"
+                                        id="trajectoryType"
+                                        name="trajectoryType"
+                                        value="walk">
+                                <label class="form-check-label" for="trajectoryType">Car Ride</label>
+                            </div>
+                        </div>
+                        <button type="submit" name="trexsubmit" class="btn btn-primary">Upload</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <!-- Bootstrap JS (with Popper.js) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
@@ -635,12 +657,6 @@ unset($db);
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.bundle.min.js"></script>
 
-<script>
-    function showElement(id) {
-        $("#" + id).toggle();
-    }
-
-</script>
 <script>
 
     function loadGPSData(filename) {
